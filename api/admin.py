f7f03c5bb2f121678884300034ccb0592daba1dd
EduardoTA <EduardoTA@live.com>
2022-03-17 20:50:42 -0300

    Changes to BankAccount and Customer ModelAdmins

--- api/admin.py
+++ api/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import BankAccount, Transaction, Customer
 
 class BankAccountAdmin(admin.ModelAdmin):
-    # list_display = ('account_number','get_balance')
-    pass
+    list_display = ('account_number','checking_sub_account_balance', 'savings_sub_account_balance')
 
 class CustomerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('customer_id', 'name', 'cpf', 'phone_number')
+    readonly_fields = ['customer_id']
 
 
 # Register your models here.
