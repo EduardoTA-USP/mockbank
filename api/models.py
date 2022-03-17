e8809f1b29021d01e76cb4044d847ffbe533bc93
EduardoTA <EduardoTA@live.com>
2022-03-17 20:48:18 -0300

    Various changes to Transaction model

--- api/models.py
+++ api/models.py
@@ -8,6 +8,8 @@ from django.contrib import admin
 
 from django.contrib.auth.models import AbstractUser
 
+from django.utils import timezone
+
 class Customer(AbstractUser):
     customer_id = models.TextField(editable=False, null=True)
     name = models.TextField()
@@ -39,9 +41,44 @@ class BankAccount(models.Model):
         return f'Name: {self.customer.name}, CPF: {self.customer.cpf}, Acc.Nº: {self.account_number}, Br.Nº: {self.branch_number}'
 
 class Transaction(models.Model):
+    CHECKING = 'CHECKING'
+    SAVINGS = 'SAVINGS'
+    SUB_ACCOUNT_CHOICES = (
+        (CHECKING, 'Checking Account'),
+        (SAVINGS, 'Savings Account'),
+    )
+
+    CREDIT = 'CREDIT'
+    DEBIT = 'DEBIT'
+    CREDIT_DEBIT_INDICATOR = (
+        (CREDIT, 'Crédito no Extrato'),
+        (DEBIT, 'Débito no Extrato')
+    )
+
     transaction_id = models.BigAutoField(primary_key=True)
-    transaction_amount = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
-    transaction_name = models.TextField()
-    transaction_date = models.DateField(default=datetime.date.today)
-    partie1 = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name='partie1')
-    partie2 = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name='partie2')
+
+    # Como é só um MVP, usa-se floats para valor de transação
+    # mas o certo é usar um integer cuja visualização é com vírgula
+    transaction_amount = models.FloatField()
+    creditDebitType = models.TextField(choices=CREDIT_DEBIT_INDICATOR, default=DEBIT)
+
+    transaction_description = models.TextField()
+    transaction_date = models.DateTimeField(default=timezone.now())
+
+    first_partie_bank_account = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name='first_partie')
+    first_partie_type_of_sub_account = models.TextField(choices=SUB_ACCOUNT_CHOICES, default=CHECKING)
+
+    # Não achamos necessário para o MVP implementar a terceira pessoa da transação
+    # third_partie_bank_name = ...
+    # third_partie_bank_branch_number = ...
+    # third_partie_bank_account_number = ...
+    # third_partie_type_of_sub_account
+    # third_partie_cpf = ...
+
+    def __str__(self):
+        return f'''{self.creditDebitType}, 
+        ${self.transaction_amount}, 
+        Name: {self.first_partie_bank_account.customer.name}, 
+        CPF: {self.first_partie_bank_account.customer.cpf}, 
+        Acc.Nº: {self.first_partie_bank_account.account_number}, 
+        Br.Nº: {self.first_partie_bank_account.branch_number}'''
