07e86d339a4bb2729887e3c6354d8a98ad30e352
EduardoTA <EduardoTA@live.com>
2022-03-23 23:18:33 -0300

    Disabled unused signals

--- api/signals.py
+++ api/signals.py
@@ -1,6 +1,39 @@
-from api.models import Customer
-from django.db.models.signals import post_save
+from datetime import datetime
+from api.models import Transaction, BankAccount, Customer
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+from django.db.models import Q
+
+from django.utils import timezone
+
+# def update_balance(bank_account: BankAccount):
+#     transactions = Transaction.objects.filter(first_partie_bank_account=bank_account).filter(Q(transaction_date__lte=timezone.now()))
+
+#     checking_sub_account_balance = 0.0
+#     savings_sub_account_balance = 0.0
+#     for transaction in transactions:
+#         if transaction.first_partie_type_of_sub_account == 'CHECKING':
+#             checking_sub_account_balance += transaction.transaction_amount
+#         elif transaction.first_partie_type_of_sub_account == 'SAVINGS':
+#             savings_sub_account_balance += transaction.transaction_amount
+#         else:
+#             raise(f'Invalid type of sub-account {transaction.first_partie_type_of_sub_account}')
+    
+#     bank_account_object = BankAccount.objects.filter(account_id = bank_account.account_id)
+#     bank_account_object.update(checking_sub_account_balance=checking_sub_account_balance)  
+#     bank_account_object.update(savings_sub_account_balance=savings_sub_account_balance)  
+
+# @receiver(pre_delete, sender=Transaction)
+# def pre_delete_update_balance(sender, instance, **kwargs):
+#     bank_account = instance.first_partie_bank_account
+#     transaction = Transaction.objects.filter(pk=str(instance.pk))
+#     transaction.update(transaction_amount=0.0)
+#     update_balance(bank_account)
+
+# @receiver(post_save, sender=Transaction)
+# def post_save_update_balance(sender, instance, created, **kwargs):
+#     bank_account = instance.first_partie_bank_account
+#     update_balance(bank_account)
 
 import secrets
 def random_gen():
