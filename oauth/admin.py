02bd1b7ebb3c71e62c6e076ae80d078c51696084
EduardoTA <EduardoTA@live.com>
2022-03-23 23:12:32 -0300

    Updated OAuth2UserConsents admin

--- oauth/admin.py
+++ oauth/admin.py
@@ -6,7 +6,11 @@ class OAuth2ClientAdmin(admin.ModelAdmin):
     list_display = ('user', 'client_name')
     readonly_fields = ('client_id', 'client_secret')
 
+class OAuth2UserConsentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in OAuth2UserConsent._meta.get_fields() if field.name not in ['consent_id']]
+    readonly_fields = ['consent_id']
+
 admin.site.register(OAuth2Client, OAuth2ClientAdmin)
 admin.site.register(OAuth2Token)
 admin.site.register(OAuth2Code)
-admin.site.register(OAuth2UserConsent)
+admin.site.register(OAuth2UserConsent, OAuth2UserConsentAdmin)
