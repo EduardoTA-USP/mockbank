d0d803da21912396f9645ba1a2b59e5b055b40c0
EduardoTA <EduardoTA@live.com>
2022-03-23 22:58:25 -0300

    Edited authorization request and issue token endpoints

--- oauth/urls.py
+++ oauth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import authorize, issue_token
 
 urlpatterns = [
-    path('authorize/', authorize, name='authorize'),
-    path('token/', issue_token, name='issue_token')
+    path('auth/', authorize, name='authorize'),
+    path('token/', issue_token, name='issue_token'),
 ]
