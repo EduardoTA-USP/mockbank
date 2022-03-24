51605820bedcb31ccc17da6952437b8a8e07e3ad
EduardoTA <EduardoTA@live.com>
2022-03-23 23:23:20 -0300

    Created bank account transactions endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import account_balances
+from .views import account_balances, transactions
 
 urlpatterns = [
     path('balances/', account_balances, name='account_balances'),
+    path('transactions/', transactions, name='transactions')
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from authlib.integrations.django_oauth2 import ResourceProtector, BearerTokenVal
 from django.http import JsonResponse
 from oauth.models import OAuth2Token
 
-from api.models import BankAccount
+from api.models import BankAccount, Transaction
 
 require_oauth = ResourceProtector()
 require_oauth.register_token_validator(BearerTokenValidator(OAuth2Token))
@@ -17,4 +17,21 @@ def account_balances(request):
     response['data']['availableAmount_CHECKING'] = bank_account.checking_sub_account_balance()
     response['data']['availableAmount_SAVINGS'] = bank_account.savings_sub_account_balance()
 
+    return JsonResponse(response, status=201)
+
+@require_oauth()
+def transactions(request):
+    customer = request.oauth_token.user
+    bank_account = BankAccount.objects.filter(customer=customer).first()
+    transactions = Transaction.objects.filter(first_partie_bank_account=bank_account)
+
+    response = {}
+    response['data'] = []
+    for transaction in transactions:
+        _dict = {}
+        _dict['creditDebitType'] = transaction.creditDebitType
+        _dict['transactionName'] = transaction.transaction_description
+        _dict['amount'] = transaction.transaction_amount
+        _dict['transactionDate'] = transaction.transaction_date
+        response['data'].append(_dict)
     return JsonResponse(response, status=201)
