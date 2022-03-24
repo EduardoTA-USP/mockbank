b845d2306da16c2d034bba060d35aef623628e09
EduardoTA <EduardoTA@live.com>
2022-03-23 23:13:21 -0300

    Added consents urls

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import authorize, issue_token
+from .views import authorize, issue_token, consents
 
 urlpatterns = [
     path('auth/', authorize, name='authorize'),
     path('token/', issue_token, name='issue_token'),
+    path('consents/consents/', consents, name='consents'),
+    path('consents/consents/<str:consent_id>/', consents, name='consents'),
 ]
